84e7121739745f66e1ab1674fcfdb0369d74396c
Jonas Imping <j.imping@splintnet.de>
2022-10-27 09:13:55 +0200

    fix

--- templates/single-basement.php
+++ templates/single-basement.php
@@ -2,7 +2,7 @@
 
 get_header();
 
-$data = esc_attr(get_post_meta(get_the_ID(), 'com5anker_data', true));
+$data = get_post_meta(get_the_ID(), 'com5anker_data', true);
 
 ?>
 <section id="primary" class="content-area">
